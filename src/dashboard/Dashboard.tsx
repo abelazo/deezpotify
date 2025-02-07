b7d9ccbb3215aef2456811027e28eb435c47c614
Abel Guillen <abelazo@gmail.com>
2025-02-07 15:19:29 +0100

    feat(dashboard): Add elements to read and submit file

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,33 +1,59 @@
+import { useRef, useState } from 'react';
 import spotifyLogo from '../assets/spotify.svg'
 import './Dashboard.css'
 
 
-const spotify = {
+const spotifyInfo = {
     name: 'Spotify',
     imageUrl: spotifyLogo,
     imageSize: 50,
 };
 
 function Dashboard() {
+    const inputFile = useRef<HTMLInputElement | null>(null);
+    const [isFileSelected, setIsFileSelected] = useState(false);
+
+    const onButtonClick = () => {
+        if (inputFile.current) {
+            inputFile.current.click();
+        }
+    };
+
+    const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            console.log('Selected file:', file);
+            setIsFileSelected(true);
+        } else {
+            setIsFileSelected(false);
+        }
+    };
+
+    const onSubmit = () => {
+        console.log('Submit button clicked');
+        console.log(inputFile.current?.files[0]);
+    }
+
   return (
     <>
       <div>
         <a href="https://spotify.com" target="_blank">
           <img
               className="logo spotify"
-              src={spotify.imageUrl}
+              src={spotifyInfo.imageUrl}
               alt="Spotify logo"
               style={{
-                width: spotify.imageSize,
-                height: spotify.imageSize
+                width: spotifyInfo.imageSize,
+                height: spotifyInfo.imageSize
               }}
           />
         </a>
       </div>
-      <h1>Login</h1>
-      <p className="read-the-docs">
-        Logged into Spotify
-      </p>
+      <h1>List</h1>
+        <input onChange={onFileChange} type='file' id='file' ref={inputFile} style={{display: 'none'}}/>
+        <button onClick={onButtonClick}>Open file upload window</button>
+        <br/>
+        <button onClick={onSubmit} disabled={!isFileSelected}>Submit</button>
     </>
   )
 }
